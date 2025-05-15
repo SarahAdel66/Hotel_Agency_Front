b31fe98e664f98cae8fc27beac8674f239f6e34d
Sarah Adel <155972289+SarahAdel66@users.noreply.github.com>
2025-05-15 19:34:05 +0300

    Update DB URL

--- src/app/Services/dishes.service.ts
+++ src/app/Services/dishes.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class DishesService {
   constructor(private http: HttpClient) { }
 
-  private DB_URL = "https://hotel-40q0.onrender.com/dishes";
+  private DB_URL = "https://hotel-agency.onrender.com/dishes";
   getDishList() {
     return this.http.get(this.DB_URL);
   }
